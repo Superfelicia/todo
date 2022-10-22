bffa50e9d48ad74d5a5855624d1e2ac4a7dcdf0a
Superfelicia <felicia.ahlstrom@hotmail.se>
2022-10-22 15:02:02 +0200

    Added edit button without function

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,7 +5,8 @@ export const Task = (props) => {
             <div className={'task-box'}>
                 <h2 className={'task-name'}>{props.taskName}</h2>
                 <button className={'delete-btn'} onClick={() => props.deleteTask(props.id)}> X </button>
-                <button className={'complete-btn'} onClick={() => console.log(props.completeTask(props.id) !== props.id)}>Complete</button>
+                <button className={'complete-btn'} onClick={() => props.completeTask(props.id)} hidden={props.completed}>Complete</button>
+                <button className={'edit-btn'} onClick={''}>Edit</button>
             </div>
         )
 }
