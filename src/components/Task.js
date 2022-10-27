fdab79f5979a25948f2f467fa9607fa1a116ce55
Superfelicia <felicia.ahlstrom@hotmail.se>
2022-10-27 21:30:21 +0200

    Added and changed some styling

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,30 @@
 import '../styles/TodoStyles.css'
+import {useState} from "react";
 
 export const Task = (props) => {
+    const [hidden, setHidden] = useState(true);
+
     return (
             <div className={'task-box'}>
-                <h2>{`${props.taskName}`}</h2>
-                <button className={'delete-btn'} onClick={() => props.deleteTask(props.id)}> X </button>
-                <button className={'complete-btn'} onClick={() => props.completeTask(props.id)} hidden={props.completed}>Complete</button>
-                <button className={'edit-btn'} onClick={() => props.editingTask(props.id)} hidden={props.completed} >Edit</button>
+                <h2 className={'task-name'}>{`${props.taskName}`}</h2>
+                <button className={'open-edit-btn'} onClick={() => setHidden(set => !set)} hidden={!hidden}>Edit</button>
+                {!hidden ? <div className={'edit-btn-box'}>
+                    <button
+                        className={'delete-btn'}
+                        onClick={() => props.deleteTask(props.id)}> X
+                    </button>
+                    <button
+                        className={'complete-btn'}
+                        onClick={() => props.completeTask(props.id)}
+                        hidden={props.completed}>C
+                    </button>
+                    <button
+                        className={'edit-btn'}
+                        onClick={() => props.editingTask(props.id)}
+                        hidden={props.completed}>E
+                    </button>
+                </div>
+                    : null }
             </div>
         )
 }
